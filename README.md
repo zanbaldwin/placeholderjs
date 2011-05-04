9420fe6cb26e8064a0b3181d4e4830a62645aea0
Alexander Baldwin <zanders@zafr.net>
2011-05-04 08:59:18 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+﻿	/**
+	 * Input Autofill
+	 *
+	 * @jQuery		Plugin
+	 * @author		[Alexander Baldwin][1].
+	 * @copyright	2011, [nerv Interactive Limited][2].
+	 * @link		https://github.com/mynameiszanders/inputAutoFill
+	 */
+
+[1]: http://mynameiszanders.github.com/ "Alexander Baldwin"
+[2]: http://nerv.co.uk "nerv Interactive: Digital Agency"
---
+++ inputAutoFill.jquery.js
@@ -0,0 +1,47 @@
+﻿(function($) {
+  $.fn.autoFill = function(preClass) {
+    preClass = typeof preClass == 'string' ? preClass : false;
+    input = $(this);
+    if(input.length == 0) {
+      return false;
+    }
+    var rel = 'focused';
+    $.each(input, function(index, element) {
+      var input = $(element);
+      var defaultval = element.defaultValue;
+      input.addClass(preClass);
+      input.bind('focus', function(event) {
+        if(input.attr('rel') != rel) {
+          defaultval = input.attr('value');
+          input.attr('value', '');
+          input.attr('rel', rel);
+          input.removeClass(preClass);
+        }
+      });
+      input.bind('blur', function(event) {
+        if(input.attr('value') == '') {
+          input.attr('value', defaultval);
+          input.attr('rel', '');
+          input.addClass(preClass);
+        }
+      });
+      var form = input.parents('form');
+      if(form.length != 0) {
+        form = $(form.get(0));
+        form.bind('reset', function(event) {
+          setTimeout(function() {
+            input.attr('value', '');
+            input.trigger('blur');
+          }, 1);
+        });
+		form.bind("submit", function(event) {
+			if(input.data("required") == true && input.attr("rel") != rel) {
+				event.preventDefault();
+				input.trigger("inputRequired");
+				return false;
+			}
+		});
+      }
+    });
+  };
+})(jQuery);
